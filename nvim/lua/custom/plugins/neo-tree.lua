b5e819903abdbd7287ff2660af7ca86943bce698
deadCStler <chitranshsrivastava01@gmail.com>
2024-04-20 13:24:32 +0530

    updated: Apr 20th

---
+++ nvim/lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,37 @@
+-- Unless you are still migrating, remove the deprecated commands from v1.x
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    version = "*",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        require('neo-tree').setup {
+            close_if_last_window = true,
+            window = {
+                position = "float",
+                width = 30,
+            },
+            event_handlers = {
+                {
+                    event = "file_opened",
+                    handler = function(file_path)
+                        require("neo-tree.command").execute({ action = "close" })
+                    end
+                },
+            },
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                    show_hidden_count = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = true
+                }
+            }
+        }
+    end,
+}
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,43 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+local act = wezterm.action
+
+config.audible_bell = "Disabled"
+
+config.color_scheme = "Catppuccin Macchiato"
+
+config.font_size = 14.0
+
+config.window_decorations = 'NONE'
+config.window_close_confirmation = 'NeverPrompt'
+
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.tab_max_width = 25
+
+config.enable_wayland = false
+
+config.keys = {
+	{ key = 'Enter', mods = 'ALT',       action = act.SpawnTab 'CurrentPaneDomain' },
+	{ key = 'v',     mods = 'ALT',       action = act.ActivateCopyMode },
+	{ key = 't',     mods = 'ALT|SHIFT', action = wezterm.action.ShowLauncherArgs { flags = 'TABS' } },
+
+	-- Panes
+	{ key = 'w',     mods = 'ALT',       action = act.CloseCurrentPane({ confirm = true }) },
+	{ key = 'h',     mods = 'ALT|SHIFT', action = wezterm.action.SplitPane { direction = 'Left', size = { Percent = 50 }, } },
+	{ key = 'j',     mods = 'ALT|SHIFT', action = wezterm.action.SplitPane { direction = 'Down', size = { Percent = 50 }, } },
+	{ key = 'k',     mods = 'ALT|SHIFT', action = wezterm.action.SplitPane { direction = 'Up', size = { Percent = 50 }, } },
+	{ key = 'l',     mods = 'ALT|SHIFT', action = wezterm.action.SplitPane { direction = 'Right', size = { Percent = 50 }, } },
+
+	-- Tabs
+	{ key = 'q',     mods = 'ALT',       action = act.CloseCurrentTab({ confirm = true }) },
+	{ key = '1',     mods = 'ALT',       action = act.ActivateTab(0) },
+	{ key = '2',     mods = 'ALT',       action = act.ActivateTab(1) },
+	{ key = '3',     mods = 'ALT',       action = act.ActivateTab(2) },
+	{ key = '4',     mods = 'ALT',       action = act.ActivateTab(3) },
+	{ key = '5',     mods = 'ALT',       action = act.ActivateTab(4) },
+
+}
+
+return config
